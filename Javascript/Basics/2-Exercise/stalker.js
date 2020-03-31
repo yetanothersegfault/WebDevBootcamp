d0068c56ef1c3afde44de3c868e7ab5848bec1e0
David <david.barnes0402@gmail.com>
2020-03-31 12:28:10 -0400

    first exercise completed

---
+++ Javascript/Basics/2-Exercise/stalker.js
@@ -0,0 +1,6 @@
+var first = prompt("What is your first name?");
+var last = prompt("What is your last name?");
+var age = prompt("What is your age?");
+
+console.log("Your full name is " + first + " " + last);
+console.log("you are " + age + " years old");

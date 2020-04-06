6a52232f6dd9ca85cd1778403b5ef004fb98afce
David <david.barnes0402@gmail.com>
2020-04-06 15:15:13 -0400

    refactored to make functions for each command. Added delete function into list

--- Javascript/Arrays/1-Todo/todo.js
+++ Javascript/Arrays/1-Todo/todo.js
@@ -8,14 +8,39 @@ window.setTimeout(function(){
 	{
 		if(input === "new")
 		{
-			todos.push(prompt("Enter a new Todo"));
-		}
-		if(input === "list")
+			addTodo();
+		}else if(input === "list")
 		{
-			console.log(todos);
+			listTodos();
+		}else if(input === "delete")
+		{
+			deleteTodo();
 		}
 
 		input = prompt("What would you like to do");
 	}
 
 },500);
+
+
+function listTodos()
+{
+	console.log("**********");
+	todos.forEach(function(todo, i, list){
+		console.log(i + ": " + todo);
+	})
+	console.log("**********");
+}
+
+function addTodo()
+{
+	var toAdd = prompt("Enter a new Todo");
+	todos.push(toAdd);
+	console.log(toAdd + " added to the list");
+}
+
+function deleteTodo()
+{
+	var index = prompt("Enter index of todo to delete")
+	todos.splice(index, 1);
+}

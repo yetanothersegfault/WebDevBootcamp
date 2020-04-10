1e897870d8cd09165d476c7414fd2ae1453623c5
David <david.barnes0402@gmail.com>
2020-04-10 11:01:26 -0400

    Select and manipulate with js using the DOM

---
+++ Javascript/Dom/1-Select-Manipulate/dom.js
@@ -0,0 +1,18 @@
+var h1 = document.querySelector("h1");
+
+h1.style.color = "pink";
+
+var body = document.querySelector("body");
+var isBlue = false;
+
+setInterval(function(){
+	if(isBlue)
+	{
+		body.style.background = "white";
+	}
+	else
+	{
+		body.style.background = "#3498db";
+	}
+	isBlue = !isBlue;
+}, 1000);

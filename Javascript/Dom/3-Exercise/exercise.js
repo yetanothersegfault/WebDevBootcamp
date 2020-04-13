198b814f0ea6615993d3333968c501fd51725b78
David <david.barnes0402@gmail.com>
2020-04-13 11:48:24 -0400

    Completed the selector exercise

---
+++ Javascript/Dom/3-Exercise/exercise.js
@@ -0,0 +1,8 @@
+var one = document.getElementById("first");
+console.log(one);
+var two = document.getElementsByClassName("special")[0];
+console.log(two);
+var three = document.querySelector("p");
+console.log(three);
+var four = document.querySelectorAll("#first")[0];
+console.log(four);

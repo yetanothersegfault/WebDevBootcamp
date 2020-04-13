db40c17166de806affd769cfafdb136c140d1978
David <david.barnes0402@gmail.com>
2020-04-13 16:21:46 -0400

    get and set Attributes

---
+++ Javascript/Dom/5-Manipulate-Attributes/manipulate.js
@@ -0,0 +1,18 @@
+var link = document.querySelector("a");
+//we are going to use the console to change the anchor attributes
+//writing down the commands in comments for future reference
+
+//link.getAttribute("href");
+//will show the current attribute that is in the href attribute of the a tag
+
+//link.setAttribute("href", "https://www.dogs.com");
+//will change the attribute of href from google.com to dogs.com	
+
+//we can also do the same with images
+var img = document.querySelector("img");
+
+//img.getAttribute("src");
+//as imagined will show the current attribute that is given
+
+//img.setAttribute("src", "corgi.jpeg");
+//will set the attribute to a new value, in this case a image of a corgi

5552daf6b7230e886639093945feb86f9268996d
David <david.barnes0402@gmail.com>
2020-04-13 15:25:50 -0400

    manipulating with javascript and add/remove/toggle css classes with javascript. Also textContent and innerHTML

---
+++ Javascript/Dom/4-Manipulators/manipulate.js
@@ -0,0 +1,14 @@
+// var h1 = document.querySelector("h1");
+// h1.style.color = "yellow";
+// h1.style.border = "5px solid pink";
+
+var tag = document.getElementById("highlight");
+tag.classList.add("some-class");
+
+var p = document.querySelector("p");
+//p.classList.add("big");
+
+var ul = document.querySelector("ul");
+
+
+document.querySelector("h1").textContent = "END OF THIS LESSON";

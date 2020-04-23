2e03254909e0360d99b3a93effd34c5b68d31115
David <david.barnes0402@gmail.com>
2020-04-23 14:50:44 -0400

    finished jQuery exercise completed

---
+++ jQuery/2-Exercise/exercise.js
@@ -0,0 +1,11 @@
+//give all divs a purple background
+$("div").css("backgroundColor", "purple");
+
+//make all divs with class highlight 200px wide
+$(".highlight").css("width", "200px");
+
+//make the div with the id third have a orange border
+$("#third").css("border", "2px solid orange");
+
+//select only first div and give it font color pink
+$("div:first").css("color", "pink");

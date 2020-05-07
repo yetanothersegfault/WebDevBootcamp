9db5406c63fb76ae1a5e5027ade8e55658b1c50a
David <david.barnes0402@gmail.com>
2020-05-07 16:46:01 +0000

    Added pattern recognition and how they are sent with the request object

--- Backend/Express/1-Intro/app.js
+++ Backend/Express/1-Intro/app.js
@@ -15,5 +15,20 @@ app.get("/dog", function(req, res){
 	res.send("Meow!");
 });
 
+//this will grab any thing that follows the /r/anything format
+//use the colon to make it a pattern in express
+app.get("/r/:subredditName", function(req, res){
+	//req has every detail about the incoming request
+	//including req.params which has the string that was in the pattern field
+	console.log(req.params);
+	var subName = req.params.subredditName;
+	res.send("WELCOME TO THE " + subName.toUpperCase() + " SUBREDDIT!");
+});
+
+//catch the rest of the quiries that we don't have a mapping for
+app.get("*", function(req, res){
+	res.send("Star Page");
+});
+
 //Tell express to listen for requests (start the server)
 app.listen(3000, process.env.IP);
